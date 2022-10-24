5e52ee2e6cf38e0f2e485975249222afab9caadc
arefinnuman <arefinnuman2000@gmail.com>
2022-10-24 20:49:56 +0600

    first commit

---
+++ src/Contexts/UserContext.js
@@ -0,0 +1,55 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithRedirect,
+  signOut,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { app } from "../Firebase/firebase.config";
+
+export const AuthContext = createContext();
+
+const auth = getAuth(app);
+
+const UserContext = ({ children }) => {
+  const [user, setUser] = useState([]);
+  //   const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleLogIn = (googleProvider = new GoogleAuthProvider()) => {
+    return signInWithRedirect(auth, googleProvider);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubcribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      //   setLoading(true);
+    });
+
+    return () => unsubcribe;
+  }, []);
+
+  const authInfo = { user, logIn, logOut, createUser, googleLogIn };
+
+  return (
+    <div>
+      <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    </div>
+  );
+};
+
+export default UserContext;
